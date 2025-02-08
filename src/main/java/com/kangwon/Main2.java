9058674a3e29bb420523dea75c95cea4b2f5a00c
kimjunhee <manuna530@gmail.com>
2025-02-08 17:23:17 +0900

    REDIS Command

--- src/main/java/com/kangwon/Main2.java
+++ src/main/java/com/kangwon/Main2.java
@@ -3,6 +3,8 @@ package com.kangwon;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Main2 {
@@ -11,7 +13,25 @@ public class Main2 {
 
         try (var jedisPool = new JedisPool("127.0.0.1", 6379)) {
             try(Jedis jedis = jedisPool.getResource()) {
+                // hash
+                jedis.hset("users:2:info", "name", "greg2");
 
+                var map = new HashMap<String, String>();
+                map.put("email", "greg2@gmail.com");
+                map.put("phone", "010-1234-5678");
+
+                jedis.hset("users:2:info", map);
+
+                // hdel
+                jedis.hdel("users:2:info", "phone");
+
+                // get, mget
+                System.out.println(jedis.hget("users:2:info", "email"));
+                Map<String, String> user2Info =  jedis.hgetAll("users:2:info");
+                user2Info.forEach((k, v) -> System.out.printf("%s %s%n", k, v));
+
+                // hinter
+                jedis.hincrBy("users:2:info", "vitis", 1);
             }
         }
     }
